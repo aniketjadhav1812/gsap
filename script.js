95d3ac3963d36f60eb06a1c8972246899ab7b52a
aniketjadhav1812 <128202035+aniketjadhav1812@users.noreply.github.com>
2024-08-14 00:34:24 +0530

    New files added.
    About page added.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let tl = gsap.timeline();
 
 const loading = () => {
-  let loader = document.getElementById("loader"); 
+  let loader = document.getElementById("loader");
   let count = document.getElementById("count");
   let i = 0;
   let intervalId = setInterval(function () {
@@ -11,54 +11,88 @@ const loading = () => {
     } else {
       clearInterval(intervalId);
       count.innerHTML = `<h1>100%</h1>`;
-      gsap.to(loader,{
-          duration: 1,
-          opacity: 0,
-          display: "none",
-        })
+      gsap.to(loader, {
+        duration: 1,
+        opacity: 0,
+        display: "none",
+      });
     }
-}, 30);
-gsap.to("#bar",{
-  height:"0%",
-  duration:3,
-  ease: 'ease-in-out',
-});
-gsap.to("#loader h1",{
-  bottom: "50%",
-  duration:2.9,
-});
-};
-loading();
-const width = window.innerWidth;
+  }, 30);
+  gsap.to("#bar", {
+    height: "0%",
+    duration: 3,
+    ease: "ease-in-out",
+  });
+  gsap.to("#loader h1", {
+    bottom: "50%",
+    duration: 2.9,
+  });
+  const width = window.innerWidth;
 
   if (width < 500) {
-    tl.to("#images img",{
+    tl.to("#images img", {
       width: "60%",
-      rotate: "10deg",    
+      rotate: "10deg",
       duration: 1,
-      delay:3.7,
-  })
+      delay: 3.7,
+    });
   } else {
-    tl.to("#images img",{
+    tl.to("#images img", {
       width: "25%",
-      rotate: "15deg",    
+      rotate: "15deg",
       duration: 1,
-      delay:3.7,
-  })
+      delay: 3.7,
+    });
   }
-tl.from("#text h1",{
+  tl.from("#text h1", {
     paddingLeft: "200%",
-    rotate: "15deg",    
+    rotate: "15deg",
     duration: 0.5,
-    stagger:0.5,
-})
-tl.from(".links a ,.logo h1",{
-  y: -100,
-  duration: .5,  
-  stagger: .1, 
-  opacity: 0,
-  delay: -2 
-})
+    stagger: 0.5,
+  });
 
+  tl.from(".links a ,.logo h1", {
+    y: -100,
+    duration: 0.5,
+    stagger: 0.1,
+    opacity: 0,
+    delay: -2,
+  });
+};
 
+const About = () => {
+  const navAnimation = () => {
+    tl.from(".links a ,.logo h1", {
+      y: -100,
+      duration: 0.5,
+      stagger: 0.1,
+      opacity: 0,
+    });
+  };
+  navAnimation();
 
+  let para = document.querySelector("#about p");
+    let clutter = '';
+    let words = para.textContent.split(" ");
+    words.forEach((word) => {
+      clutter += ` <span>${word}</span> `;
+    });
+    para.innerHTML = clutter;
+
+  gsap.from("#heading",{
+    x: 500,
+    opacity:0,
+    duration: 0.5,
+    });
+    let tl1 = gsap.timeline();
+    tl1.from('#about p',{
+      x: -500,
+      opacity:0,
+      duration: 0.5,
+      });
+  tl1.to("#about span",{
+    opacity:1,
+    duration: 0.5,
+    stagger: 0.1,
+  })
+};
